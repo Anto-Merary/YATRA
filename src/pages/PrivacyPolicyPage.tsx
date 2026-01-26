1af9b09cbaab476567fabb12a4fcde731aec1f7c
andrwdom <m.andrewdominic9b@gmail.com>
2026-01-26 08:07:36 +0530

    made the features of yatra page more darker so that the content on top is highlighted

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from "react";
+  import { useLayoutEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { NoiseOverlay } from "@/components/NoiseOverlay";
