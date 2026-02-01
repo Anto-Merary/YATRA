5e424f7ae350c882ec935553e4a897bdd37b44ba
Anto-Merary <meraryanto@gmail.com>
2026-02-01 09:27:46 +0530

    payment gateway shi

--- vite.config.ts
+++ vite.config.ts
@@ -62,6 +62,7 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, "index.html"),
         mobile: path.resolve(__dirname, "mobile.html"),
+        homepage: path.resolve(__dirname, "homepage.html"),
       },
     },
   },
